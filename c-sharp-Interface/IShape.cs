67d1e6610598a25d4459dfc7b7e881ab2804990b
omranali2111 <aljabri1234@hotmail.com>
2023-06-21 12:45:04 +0400

    Interfaces

---
+++ c-sharp-Interface/IShape.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp_Interface
+{
+    internal interface IShape
+    {
+        public double getArea();
+    }
+
+    class Rectangle: IShape
+    {
+        double hight;
+        double Width;
+        public Rectangle(double hight, double width)
+        {
+            this.hight = hight;
+            this.Width = width;
+        }
+        public double getArea()
+        {
+            return hight* Width;
+        }
+    }
+    class Circle: IShape
+    {
+       
+        double Radius;
+        public Circle(double Radius)
+        {
+            this.Radius = Radius;
+        }
+        public  double getArea()
+        {
+            return (Math.PI * Radius * Radius);
+        }
+    }
+}
---
+++ c-sharp-Interface/Program.cs
@@ -0,0 +1,14 @@
+﻿using c_sharp_Interface;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+     
+
+        IShape[] shape =  { new Circle(2.3), new Rectangle(2.3, 3.5) };
+
+        Console.WriteLine($"the Area of Circle is:{shape[0].getArea()}");
+        Console.WriteLine($"the Area of Rectangle is:{shape[1].getArea()}");
+    }
+}
