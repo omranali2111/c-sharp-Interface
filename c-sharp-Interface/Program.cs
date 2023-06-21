27514bc42ec99463c7685a6d3da50991f6e2d421
omranali2111 <aljabri1234@hotmail.com>
2023-06-21 13:49:50 +0400

    accepting the parameters from user in main

--- c-sharp-Interface/Program.cs
+++ c-sharp-Interface/Program.cs
@@ -4,9 +4,17 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-     
+        Console.WriteLine("enter the radius of the circle");
+        double a=double.Parse(Console.ReadLine());
 
-        IShape[] shape =  { new Circle(2.3), new Rectangle(2.3, 3.5) };
+        Console.WriteLine("enter the hight of the rectangle");
+        double b= double.Parse(Console.ReadLine());
+
+        Console.WriteLine("enter the width of the rectangle");
+        double c = double.Parse(Console.ReadLine());
+
+
+        IShape[] shape = { new Circle(a), new Rectangle(b, c) };
 
         Console.WriteLine($"the Area of Circle is:{shape[0].getArea()}");
         Console.WriteLine($"the Area of Rectangle is:{shape[1].getArea()}");
